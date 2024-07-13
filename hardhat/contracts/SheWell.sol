c1f52b8f56d93e6b5314ca4263eed940a9c8fb58
Gonzalo Barría <gonzalobarriamarchant@gmail.com>
2024-07-13 11:40:35 +0200

    adding more data to save

--- hardhat/contracts/SheWell.sol
+++ hardhat/contracts/SheWell.sol
@@ -1,14 +1,73 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract SheWell {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SheWell is Ownable {
+  using Counters for Counters.Counter;
+
+  /// @dev Symptom id counter
+  Counters.Counter symptomId;
+
+  /// @dev Menstrual Cycle Tracking id counter
+  Counters.Counter mctId;
+
   struct User {
     string name;
     string content;
     bool isExists;
   }
 
+  struct Suggestion {
+    uint8 startDay;
+    uint8 endDay;
+    string content;
+  }
+
+  struct Symptom {
+    uint256 id;
+    string name;
+    string emoji;
+  }
+
+  /// @dev id of the Symptoms
+  struct Symp {
+    uint256 id;
+    uint8 level;
+  }
+
+  struct Intercourse {
+    bool useCondom;
+    bool hasOrgasm;
+    uint8 count;
+  }
+
+  struct MenstrualCycleTracking {
+    uint256 id;
+    uint256 startDate;
+    uint256 endDate;
+    bool isBegin;
+    bool isEnd;
+    uint8 bleedingLevel;
+    Intercourse intercourse;
+    string notes;
+  }
+
+  Symptom[] private symptoms;
+  Suggestion[] suggestions;
+
   constructor() {}
+
+  modifier onlyUser() {
+    require(!userInfo[msg.sender].isExists, "User not registered");
+    _;
+  }
+
+  mapping(address => MenstrualCycleTracking[]) mctList;
+
+  /// @dev map id mct to symp
+  mapping(address => mapping(uint256 => Symp[])) myMCTSymptoms;
 
   mapping(address => User) userInfo;
 
@@ -22,5 +81,72 @@ contract SheWell {
 
   function getMyUserInfo() public view returns (User memory) {
     return userInfo[msg.sender];
+  }
+
+  function addSuggestion(
+    uint8 _startDay,
+    uint8 _endDay,
+    string memory _content
+  ) public onlyOwner {
+    suggestions.push(
+      Suggestion({startDay: _startDay, endDay: _endDay, content: _content})
+    );
+  }
+
+  function addSymptom(
+    string memory _name,
+    string memory _emoji
+  ) public onlyOwner {
+    uint256 id = symptomId.current();
+    symptoms.push(Symptom({id: id, name: _name, emoji: _emoji}));
+    symptomId.increment();
+  }
+
+  function getSuggestions() public view onlyUser returns (Suggestion[] memory) {
+    return suggestions;
+  }
+
+  function getSymptoms() public view returns (Symptom[] memory) {
+    return symptoms;
+  }
+
+  function addMySymptom(
+    uint256 _startDate,
+    uint256 _endDate,
+    bool _isBegin,
+    bool _isEnd,
+    uint8 _bleedingLevel,
+    Intercourse memory _intercourse,
+    string memory _notes,
+    Symp[] memory _symptoms
+  ) public {
+    uint256 id = mctId.current();
+
+    MenstrualCycleTracking memory mct = MenstrualCycleTracking({
+      id: id,
+      startDate: _startDate,
+      endDate: _endDate,
+      isBegin: _isBegin,
+      isEnd: _isEnd,
+      bleedingLevel: _bleedingLevel,
+      intercourse: _intercourse,
+      notes: _notes
+    });
+
+    mctId.increment();
+
+    mctList[msg.sender].push(mct);
+
+    for (uint i = 0; i < _symptoms.length; i++) {
+      myMCTSymptoms[msg.sender][id].push(_symptoms[i]);
+    }
+  }
+
+  function getMCTList() public view returns (MenstrualCycleTracking[] memory) {
+    return mctList[msg.sender];
+  }
+
+  function getMyMCTSymptomsId(uint256 _id) public view returns (Symp[] memory) {
+    return myMCTSymptoms[msg.sender][_id];
   }
 }
--- hardhat/test/SheWell.ts
+++ hardhat/test/SheWell.ts
@@ -25,5 +25,71 @@ describe("SheWell", function () {
 
       assert(userInfo.name, name)
     })
+
+    it("Add Symptom", async () => {
+      const { sheWell } = await loadFixture(deploySheWell)
+      const name = "Acalorada"
+      const emoji = "🩸"
+
+      await sheWell.addSymptom(name, emoji)
+      const symptoms = await sheWell.getSymptoms()
+
+      assert.equal(symptoms[0].name, name)
+    })
+
+    it("Add Sugestion", async () => {
+      const { sheWell } = await loadFixture(deploySheWell)
+      const start = 1
+      const end = 3
+      const content = "Acalorada"
+
+      await sheWell.addSuggestion(start, end, content)
+      const suggestions = await sheWell.getSuggestions()
+
+      assert.equal(suggestions[0].content, content)
+    })
+
+    it("Add My Symptoms", async () => {
+      const { sheWell } = await loadFixture(deploySheWell)
+      const startDate = new Date().getTime()
+      const endDate = new Date().getTime()
+
+      const symptomsUser = [
+        {
+          id: 1,
+          level: 2,
+        },
+        {
+          id: 2,
+          level: 4,
+        },
+      ]
+
+      const intercourse = {
+        useCondom: false,
+        hasOrgasm: true,
+        count: 2,
+      }
+
+      await sheWell.addMySymptom(
+        startDate,
+        endDate,
+        false,
+        false,
+        3,
+        intercourse,
+        "",
+        symptomsUser,
+      )
+
+      const myMCTList = await sheWell.getMCTList()
+
+      assert.equal(myMCTList.length, 1)
+
+      const myMCTSymp = await sheWell.getMyMCTSymptomsId(myMCTList[0].id)
+
+      assert.equal(myMCTSymp[1].level, BigInt("4"))
+      assert.equal(myMCTSymp[1].id, BigInt("2"))
+    })
   })
 })
