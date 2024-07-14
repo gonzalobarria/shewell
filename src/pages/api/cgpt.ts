5964d135cafe7c70ab746fe7e19ee19b17d61cc8
Gonzalo Barría <gonzalobarriamarchant@gmail.com>
2024-07-14 03:51:41 +0200

    gpt query

--- src/pages/api/cgpt.ts
+++ src/pages/api/cgpt.ts
@@ -18,17 +18,15 @@ export default async function handler(
   if (!prompt) return res.status(400).json({ message: "Prompt is required" })
 
   try {
-    // const completion = await openai.chat.completions.create({
-    //   messages: [{ role: "system", content: prompt }],
-    //   model: "gpt-3.5-turbo-16k",
-    // })
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: prompt }],
+      model: "gpt-3.5-turbo-16k",
+    })
 
-    // const text = completion.choices[0].message.content
-    return res
-      .status(200)
-      .json({
-        text: "Para una mujer nacida el 20 de enero de 1987, bajo el signo zodiacal Acuario, en el día 15 de su periodo, te recomendaría llevar a cabo actividades deportivas como natación, yoga o pilates para relajar tu mente y cuerpo. También te sugiero dedicar tiempo a actividades intelectuales como la lectura de un buen libro, la escritura o incluso participar en debates. Estas actividades te ayudarán a canalizar tu energía de forma positiva y a aprovechar al máximo tu día.",
-      })
+    const text = completion.choices[0].message.content
+    return res.status(200).json({
+      text,
+    })
   } catch (error: any) {
     return res
       .status(500)
